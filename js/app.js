3eed213c6a2b2eb2371a8b810709302625e49f02
kuczynskil <l.kuczynski95@gmail.com>
2021-04-29 19:37:48 +0200

    Move getTasks function to separate file

--- js/app.js
+++ js/app.js
@@ -1,27 +1,5 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
-import {API_KEY, API_URL} from "./constants";
-
-export const getTasks = async (successCallback) => {
-  try {
-    const response = await fetch(`${API_URL}/tasks`, {
-      headers: {
-        Authorization: API_KEY,
-      },
-    });
-
-    const data = await response.json();
-
-    if (data.error || typeof successCallback === 'function') {
-      throw new Error('Błąd!');
-    }
-
-    successCallback(data.data);
-
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 function App() {
   return <h1>Hello World</h1>
