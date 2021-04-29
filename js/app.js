bea0f76bd0ecdfd23b7d197827de996299725f6d
kuczynskil <l.kuczynski95@gmail.com>
2021-04-29 19:34:05 +0200

    Add getTasks function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {API_KEY, API_URL} from "./constants";
+
+export const getTasks = async (successCallback) => {
+  try {
+    const response = await fetch(`${API_URL}/tasks`, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    });
+
+    const data = await response.json();
+
+    if (data.error || typeof successCallback === 'function') {
+      throw new Error('Błąd!');
+    }
+
+    successCallback(data.data);
+
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 function App() {
   return <h1>Hello World</h1>
