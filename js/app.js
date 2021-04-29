a632030bfbd7b6abd3d7711eb53fabbc61cecb06
kuczynskil <l.kuczynski95@gmail.com>
2021-04-29 19:43:09 +0200

    Add getTasks to main App

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
+import {getTasks} from "./tasks";
 
 function App() {
-  return <h1>Hello World</h1>
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        getTasks(setTasks);
+    }, [])
+
+    console.log(tasks);
+    return <h1>Hello World</h1>
 }
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
